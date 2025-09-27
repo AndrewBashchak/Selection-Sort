1c12760aa35939009d1295f7136587e9863d76c0
Bloger_G.A.P. <g.a.p.bloger@gmail.com>
2025-09-27 19:44:45 +0300

    The code

---
+++ selectionSort.js
@@ -0,0 +1,48 @@
+const length = 10000;
+const min = -10000;
+const max = 10000;
+let randomNumbersArray = [];
+
+for (let i = 0; i < length; i++) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumbersArray.push(randomNumber);
+};
+
+function formatArray(arr) {
+    return arr.map((n, i) => `[cell - ${i + 1}, value - ${n}]`);
+};
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            };
+        };
+
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    };
+
+    return arr;
+};
+
+const startTime = performance.now();
+const sortedArray = selectionSort(randomNumbersArray);
+const endTime = performance.now();
+
+const formattedUpper = formatArray(sortedArray);
+console.log("Sorted array to upper:", formattedUpper);
+
+sortedArray.reverse();
+
+const formattedLower = formatArray(sortedArray);
+console.log("Sorted array to lower:", formattedLower);
+
+const durationMs = endTime - startTime;
+
+if (durationMs >= 1000) {
+    console.log(`Sorting time: ${(durationMs / 1000).toFixed(3)} seconds`);
+} else {
+    console.log(`Sorting time: ${durationMs.toFixed(3)} ms`);
+};
